c3aa3a7f934fe91b64cb962a03a3f975810ccb30
Eric Agbayani <eric.agbayani@yahoo.com>
2022-05-23 19:41:00 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,24 +10,83 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+const quotes = [
+  {  quote: "Houston, we have a problem", 
+     source: "Jim Lovell (Tom Hanks)",
+     citation: "Apollo - Movie" ,
+     year: 1995
+ },
+ {
+   quote: "You shall not pass!",
+   source: "Gandalf (Ian Mckellen)",
+   citation: "Lord of the Rings: The Fellowship of the Ring - Movie",
+   year: 2002
+ },
+ {
+   quote: "May the Force be with you",
+   source: "Han Solo (Harrison Ford)",
+   citation: "Star Wars: A New Hope - Movie",
+   year: 1977
+ },
+ {
+   quote: "Hello, my name is Inigo Montoya. You killed my father. Prepare to die.",
+   source: "Inigo Montoya(Mandy Patinkin)",
+   citation: "The Princess Bride - Movie ",
+   year: 1987
+ },
+ {
+   quote: "I'll be back.",
+   source: "Terminator (Arnold Schwarzenegger)",
+   citation: "The Terminator - Movie",
+   year: 1984
+ }
+];
 
 
+function getRandomQuote() {
+ let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+ return randomQuote; //returns a random quote from the `quotes` array 
+};
 
 /***
- * `getRandomQuote` function
+* `printQuote` function
 ***/
+function printQuote() { 
+ let quoteSelected = getRandomQuote();
+ printQuote =  `
+   <p class="quote"> ${quoteSelected.quote} </p>
+   <p class="source"> ${quoteSelected.source}
+ `
+ if (quoteSelected.citation !== undefined) {
+   printQuote += `
+   <span class="citation">${quoteSelected.citation}</span>`
+ } else {
+   printQuote += `</p>`
+ }
+
+ if (quoteSelected.year !== undefined) {
+   printQuote += `
+   <span class="year"> ${quoteSelected.year} </span></p>`
+ } else {
+   print += `</p>`
+ }
+ return printQuote;
+};
+
+document.querySelector('main').innerHTML = printQuote();
+
+
+
+ // 6. set the innerHTML of the quote-box div to equal the 
+ // complete HTML string
+
+
 
 
 
 /***
- * `printQuote` function
-***/
-
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
+* click event listener for the print quote button
+* DO NOT CHANGE THE CODE BELOW!!
 ***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
